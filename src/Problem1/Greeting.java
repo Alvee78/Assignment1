64de8c24f8e71a06d842eb852fdcbf103f69b94d
Alvee <alvee.rucse@gmail.com>
2023-07-18 13:38:18 +0600

    first commit

---
+++ src/Problem1/Greeting.java
@@ -0,0 +1,12 @@
+public class Greeting {
+    public void english(){
+        System.out.println("Hello");
+    }
+    public void bangla(){
+        System.out.println("স্বাগতম");
+    }
+    public void japanese(){
+        System.out.println("こんにちは");
+    }
+
+}
---
+++ src/Problem1/GreetingRunner.java
@@ -0,0 +1,8 @@
+public class GreetingRunner {
+    public static void main(String[] args) {
+        Greeting greeting = new Greeting();
+        greeting.bangla();
+        greeting.english();
+        greeting.japanese();
+    }
+}
